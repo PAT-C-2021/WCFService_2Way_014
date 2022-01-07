9030b090da0fd162b85c57f172e777a45f63c5a4
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2022-01-07 22:09:00 +0700

    Add project files.

---
+++ WCFService_2Way_2019014014/IServiceCallBack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_2019014014
+{   
+    [ServiceContract(CallbackContract = typeof(IClientCallBack))]
+    public interface IServiceCallBack
+    {
+        [OperationContract(IsOneWay = true)]
+        void gabung(string username);
+        [OperationContract(IsOneWay = true)]
+        void kirimPesan(string pesan);
+    }
+   
+}
